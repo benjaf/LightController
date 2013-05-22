725df0700d065b5e6d826c8b5194deb09e502aa0
Benjamin <bf@gmail.com>
2013-05-22 10:25:44 +0200

    Revert "Massive Rewrite"
    This reverts commit d65ec4e93869183bfde2adb4ee6572dee552ddb2.

--- Arduino/LightController/LightController.ino
+++ Arduino/LightController/LightController.ino
@@ -2,242 +2,144 @@
  * Name:	LightController.ino
  * Author:	User "benjaf" at plantedtank.net forums
  * URL:		https://github.com/benjaf/LightController
- * This example is set up for 2 channels with 5 lighting periods.
- * Anything that may require editing is labeled with ¤CHANGE¤
  */
 
-
-// ----------------------- RTC Library -----------------------
-// One could use RTCdue (https://github.com/oskarirauta/RTCdue) to enable Arduino Due support
-// I had to add following lines to RTCdue.h to make it compile without errors:
-// #define PIN_WIRE_SDA 4
-// #define PIN_WIRE_SCL 5
-//#include "RTCdue.h"
-// Alternatively, you can use Wire and RTClib (https://github.com/adafruit/RTClib) which work well with anything else:
 #include <Wire.h>
-#include "RTClib.h"
+#include <RTClib.h>
 
-// ----------------------- Constants -----------------------
-// ¤CHANGE¤
-const int MaxChannels = 2;   // Max number of channels, change if more or less are required
-const int MaxPoints = 10;    // Max number of light intensity points, change if more or less are required
+// ----------------------- Pins ----------------------- 
+// PWM Pins
+// Please note: Some PWM pins should not be used for lights -
+// On the Uno these are pins 5 and 6. They do not behave like the others
+// due to some shared timers!
+#define L1Pin 9
+#define L2Pin 10
+// ... and so on
 
-// ----------------------- Variables -----------------------
+// ----------------------- Named Values ----------------------- 
+#define CHANNELS 2		// Number of channels, change if more are required
+#define MAXPERIODS 5	// Max number of light periods, change if more are required
+
+// These are just used to make the lookup code more easily readable:
+#define START_H 0
+#define START_M 1
+#define END_H 2
+#define END_M 3
+#define INTENSITY 4
+
+// ----------------------- Variables ----------------------- 
 // RTC
 RTC_DS1307 RTC;
 
 // Time
-DateTime CurrentTime;
+int Hours, Minutes, Seconds;
 
-// ----------------------- Classes -----------------------
-// Point data type
-class Point {
-	public:
-		int Hour;
-		int Minute;
-		int Intensity;
-		
-		// Initialize Point with 24:00 OFF
-		Point() {
-			Hour = 24;
-			Minute = 0;
-			Intensity = 0;
-		}
-		
-		// Initialize Point with given values
-		Point(int h, int m, int intensity) {
-			Hour = h;
-			Minute = m;
-			Intensity = intensity;
-		}
-		
-		// Get Point time in seconds since midnight
-		long GetSeconds() {
-			return Seconds(Hour, Minute, 0);
-		}
-};
+// ----------------------- Lights ----------------------- 
+// Light period format: {HourStart, MinuteStart, HourEnd, MinuteEnd, Intensity}
 
-// Channel data type
-class Channel {
-	public:
-		int Pin;
-		int LightValue;
-		Point Schedule[MaxPoints];
-   
-   		// Initialize channel full of 24:00 OFF Points
-		Channel() {
-			Pin = 0;
-			LightValue = 0;
-			for(int point = 0; point < MaxPoints; point++) {
-				Schedule[point] = Point();
-			}
-		}
-		
-		// Add Point at given position
-		void SetPoint(int row, Point p) {
-			Schedule[row] = p;
-		}
-		
-		// Add Point to first available position (wrapper function)
-		void AddPoint(int h, int m, int intensity) {
-			AddPoint(Point(h, m, intensity));
-		}
-		
-		// Add Point to first available position
-		void AddPoint(Point p) {
-			for(int i = 0; i < MaxPoints; i++) {
-				if(Schedule[i].Hour == 24) {
-					Schedule[i] = p;
-					break;
-				}
-			}
-		}
-};
-
-// ----------------------- Lights -----------------------
-
-// Schedule Point format: (Hour, Minute, Intensity)
-// Difference in intensity between points is faded/increased gradually
-// Example: 
-// 	Channels[0].AddPoint(8, 0, 0);
-//	Channels[0].AddPoint(8, 30, 255);
-//	Channels[0].AddPoint(11, 0, 255);
-//  ...
-//
-// Explanation:
+// Gap between light periods is used to fade/increase intensity gradually
+// Example: {0, 0, 8, 0, 0}, {8, 30, 11, 0, 255} ...
+// Explanation: 
 //  00:00 - 08:00 -> Light OFF
-//  08:00 - 08:30 -> Increment light towards Fully ON
+//  08:00 - 08:30 -> Increment light towards desired intensity
 //  08:30 - 11:00 -> Light Fully ON
-//
+
 // Min intensity value: 0 (OFF)
 // Max intensity value: 255 (Fully ON)
-// If no Point is made at 00:00, channel is assumed to be OFF until first Point is encountered
-// If last Point is not 24:00, channel is assumed to be OFF after last Point is encountered
-//
-// Any Point with 24:00 will be considered last.
-// Point order must always be chronological overlaps.
-// Any row not explicitly set will contain 24:00 OFF, which is OK as long as anyting up to that point is valid.
-// On creation, all schedules are valid and OFF (all points are 24:00 OFF).
-//
-// Example: 
-// 	Channels[0].AddPoint(0, 0, 255);
-//	Channels[0].AddPoint(24, 0, 255);
-// Explanation:
-// Channel is ALWAYS FULLY ON.
-// As the second Point never ends, the remaining rows are only there to fill out the necessary space and their content does not matter.
+// First StartTime value MUST be 00:00
+// SOME EndTime value MUST be 24:00
 
-Channel Channels[MaxChannels];
+// Any row ending with 24:00 will be considered last.
+// Not all periods must be filled with meaningfull values for each channel.
+// Periods must always be chronological and without overlaps.
+// There must always be rows defined for the max number of periods, but not all have to be valid
+// Example: {{0, 0, 24, 0, 255},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0},{0, 0, 0, 0, 0}}
+// Explanation: 
+// Channel is ALWAYS FULLY ON. 
+// As the first period never ends, the remaining rows are only there to fill out the necessary space. 
 
-// Add more timing definitions here if more channels are added:
-// ¤CHANGE¤
+// Add more timing tables if more channels are added
 
-void InitializeChannels(int channels) {
-	// Channel 0: (Example)
-	// This is an example of a typical lighting schedule (Lights on 08:30 - 19:00 w. 30 minutes of sunrise / sunset added)
-	//Channels[0].Pin = 11;
-	//Channels[0].AddPoint(8, 0, 0);
-	//Channels[0].AddPoint(8, 30, 255);
-  	//Channels[0].AddPoint(19, 0, 255);
-  	//Channels[0].AddPoint(19, 30, 0);
-  	
-	// Channel 0:
-	Channels[0].Pin = 9;
-	Channels[0].AddPoint(8, 0, 0);
-	Channels[0].AddPoint(8, 30, 255);
-	Channels[0].AddPoint(11, 0, 0);
-	Channels[0].AddPoint(11, 10, 0);
-	Channels[0].AddPoint(13, 0, 0);
-	Channels[0].AddPoint(13, 10, 255);
-	Channels[0].AddPoint(20, 0, 255);
-	Channels[0].AddPoint(20, 30, 0);
-  
-	// Channel 1:
-	Channels[1].Pin = 10;
-	Channels[1].AddPoint(7, 45, 0);
-	Channels[1].AddPoint(8, 0, 255);
-	Channels[1].AddPoint(11, 0, 0);
-	Channels[1].AddPoint(11, 10, 0);
-	Channels[1].AddPoint(13, 0, 0);
-	Channels[1].AddPoint(13, 10, 255);
-	Channels[1].AddPoint(20, 30, 255);
-	Channels[1].AddPoint(20, 45, 0);
-}
+int lightMatrix[CHANNELS][MAXPERIODS][5] = 
+{
+	{
+		{0, 0, 8, 0, 0},
+		{8, 30, 11, 0, 255},
+		{11, 10, 13, 0, 0},
+		{13, 10, 20, 0, 255},
+		{20, 30, 24, 0, 0}
+	},{
+		{0, 0, 7, 45, 0},
+		{8, 0, 11, 0, 255},
+		{11, 10, 13, 0, 0},
+		{13, 10, 20, 30, 255},
+		{20, 45, 24, 0, 0}
+	}
+	// ... and so on
+};
 
-// ----------------------- Functions -----------------------
-// Update light intensity values 
-void UpdateLights(DateTime currentTime)	{
-	long now = Seconds(currentTime.hour(), currentTime.minute(), currentTime.second());	// Convert current time to seconds since midnight
-        for(int channel = 0; channel < MaxChannels; channel++) {  		// For each Channel
-		Channel* c = &Channels[channel];  // Store pointer to current channel here for easy access. Must use "c->" rather than "c." to access properties
-		for(int point = 0; point < MaxPoints; point++) {    // For each possible point
-			long pTime = c->Schedule[point].GetSeconds();	// Get time of this point
-			if(pTime > now) {				// First entry has not yet happened - this means no 00:00 was defined and cannel is assumed to be OFF
-				c->LightValue = 0;	// Set light to 0
-				break;	// Don't check the rest of the points
-			} else if(point + 1 >= MaxPoints) {	// We reached the end of the array, which means no 24:00 was defined. Assume channel is OFF
-				c->LightValue = 0;	// Set light to 0
-				break;	// There are no more points to check anyway
-			} else {
-				long pNextTime = c->Schedule[point+1].GetSeconds();	// Get time of the next point
-				if(pNextTime >= now) {	// We are currently between this point and the next
-					int preVal = c->Schedule[point].Intensity;		// Old light value
-					int postVal = c->Schedule[point+1].Intensity;	// New light value
-					int tDur = pNextTime - pTime; 			// Transition duration
-					int intensityDiff = postVal - preVal;	// Difference in light intesity between points
+// Current light intensity table
+// Number of values should match number of channels (i.e. {0,0,0} for 3 channels)
+int lightValue[CHANNELS] = {0, 0};
+
+// ----------------------- Functions ----------------------- 
+void UpdateLights(long seconds)		// Update light intensity values
+{
+	for(int channel = 0; channel < CHANNELS; channel++) {  		// For each Channel
+		for(int period = 0; period < MAXPERIODS; period++) {    // For each possible period
+			long pEnd = GetSeconds(lightMatrix[channel][period][END_H], lightMatrix[channel][period][END_M], 0);	// Get period end time in seconds
+			if(pEnd >= seconds) {				// Period is currently happening
+				lightValue[channel] = lightMatrix[channel][period][INTENSITY];	// Set light to defined value
+				break;		// Found correct period, don't check the rest
+			} else {		// Period has not yet happened
+				long pNextStart = GetSeconds(lightMatrix[channel][period+1][START_H], lightMatrix[channel][period+1][START_M], 0); // Get next period start time in seconds
+				if(pNextStart > seconds) {					// Currently in between periods
+					int preVal = lightMatrix[channel][period][INTENSITY];		// Old light value
+					int postVal = lightMatrix[channel][period+1][INTENSITY];	// New light value
+					int tDur = pNextStart - pEnd; 			// Transition duration
+					int intensityDiff = postVal - preVal;	// Difference in light intesity between periods
 					if(intensityDiff > 0) {		// Intensity increasing
-						c->LightValue = (int)((now - pTime) * ((float)intensityDiff / tDur));		// Light value
+						lightValue[channel] = (int)((seconds - pEnd) * ((float)intensityDiff / tDur));		// Light value
 					} else {					// Intensity decreasing
-						c->LightValue = preVal - (int)((now - pTime) * ((float)(0-intensityDiff) / tDur));	// Light value
+						lightValue[channel] = preVal - (int)((seconds - pEnd) * ((float)(0-intensityDiff) / tDur));	// Light value
 					}
-					break;	// Found correct points, don't check the rest
+					break;	// Found correct period, don't check the rest
 				}
-				// If we get this far, the correct points were not yet found. Retry with next point.
 			}
 		}
-		analogWrite(c->Pin, c->LightValue);	// Write value to pin
 	}
 }
 
-// Convert HH:mm:ss -> Seconds since midnight
-long Seconds(int hours, int minutes, int seconds) {
+long GetSeconds(int hours, int minutes, int seconds)	// Convert HH:mm:ss -> Seconds since midnight
+{
 	return ((long)hours * 60 * 60) + (minutes * 60) + seconds ;
 }
 
-// ----------------------- Setup -----------------------
-void setup() {
-	// Initialize channel schedules
-	InitializeChannels(MaxChannels);
-	
+// ----------------------- Setup ----------------------- 
+void setup()
+{
+	// Set analog pins
+	pinMode(L1Pin, OUTPUT);
+	pinMode(L2Pin, OUTPUT);
+	// ... and so on
+
 	// Clock
 	Wire.begin();
 	RTC.begin();
-	//RTC.adjust(DateTime(__DATE__, __TIME__));  // Set RTC time to sketch compilation time, only use for 1 (ONE) run. Will reset time at each device reset!
-
-	// Debug monitor
-	//Serial.begin(9600);
+	//RTC.adjust(DateTime(__DATE__, __TIME__));  // Set RTC time, only use for 1 (ONE) run. Will reset time at each device reset!
 }
 
-// ----------------------- Loop -----------------------
-void loop() {
-	// Get current time
-	CurrentTime = RTC.now();
-        
-	// Update lights
-	UpdateLights(CurrentTime);
-	
-        // Debug
-        /*
-        delay(1000);
-        Serial.print(CurrentTime.hour());
-        Serial.print(":");
-        Serial.print(CurrentTime.minute());
-        Serial.print(":");
-        Serial.println(CurrentTime.second());
-        for(int i = 0; i < MaxChannels; i++) {
-          Serial.print("Channel ");
-          Serial.print(i);
-          Serial.print(": ");
-          Serial.println(Channels[i].LightValue);
-        }
-        */
+// ----------------------- Loop ----------------------- 
+void loop(){
+	// Get Time
+	DateTime now = RTC.now();
+	Hours = now.hour();
+	Minutes = now.minute();
+	Seconds = now.second();
+  
+	// Update actual light intensity to match values defined in lightValue[]
+	UpdateLights(GetSeconds(Hours, Minutes, Seconds));
+	analogWrite(L1Pin, lightValue[0]);
+	analogWrite(L2Pin, lightValue[1]);
+	// ... and so on
 }
